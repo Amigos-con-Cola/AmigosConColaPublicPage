fa13a0571df295f793af24fcd1c4c96f59abcdb6
Cristopher Villa <cgvilla@espol.edu.ec>
2024-07-29 21:35:59 -0500

    chore: Update Steper.vue component styles

--- src/components/ui/VueComponents/Steper.vue
+++ src/components/ui/VueComponents/Steper.vue
@@ -1,12 +1,12 @@
 <template>
   <ol
-    class="md:px-10 flex items-center w-full text-sm font-medium text-center text-gray-500 dark:text-gray-400 sm:text-base"
+    class="md:px-10 flex items-center w-full text-sm font-medium text-center text-gray-500 sm:text-base"
   >
     <li
-      class="flex md:w-full items-center text-blue-600 dark:text-blue-500 sm:after:content-[''] after:w-full after:h-1 after:border-b after:border-gray-200 after:border-1 after:hidden sm:after:inline-block after:mx-6 xl:after:mx-10 dark:after:border-gray-700"
+      class="flex md:w-full items-center text-primary sm:after:content-[''] after:w-full after:h-1 after:border-b after:border-gray-200 after:border-1 after:hidden sm:after:inline-block after:mx-6 xl:after:mx-10"
     >
       <span
-        class="flex items-center after:content-['/'] sm:after:hidden after:mx-2 after:text-gray-200 dark:after:text-gray-500"
+        class="flex items-center after:content-['/'] sm:after:hidden after:mx-2 after:text-gray-200"
       >
         <svg
           class="w-3.5 h-3.5 sm:w-4 sm:h-4 me-2.5"
@@ -23,10 +23,10 @@
       </span>
     </li>
     <li
-      class="flex md:w-full items-center after:content-[''] after:w-full after:h-1 after:border-b after:border-gray-200 after:border-1 after:hidden sm:after:inline-block after:mx-6 xl:after:mx-10 dark:after:border-gray-700"
+      class="flex md:w-full items-center after:content-[''] after:w-full after:h-1 after:border-b after:border-gray-200 after:border-1 after:hidden sm:after:inline-block after:mx-6 xl:after:mx-10"
     >
       <span
-        class="flex items-center after:content-['/'] sm:after:hidden after:mx-2 after:text-gray-200 dark:after:text-gray-500"
+        class="flex items-center after:content-['/'] sm:after:hidden after:mx-2 after:text-gray-200"
       >
         <span class="me-2">2</span>
         Trabajo <span class="hidden sm:inline-flex">/Estudios</span>
@@ -34,7 +34,7 @@
     </li>
     <li class="flex items-center">
       <span class="me-2">3</span>
-      Confirmation
+      Generales
     </li>
   </ol>
 </template>
